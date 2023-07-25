87e77bbb2b0a6533707462019723b822924ca276
Aliona Kovalenko <kovalenkoalena789@gmail.com>
2023-07-25 12:36:23 +0300

    update

--- sendMail.php
+++ sendMail.php
@@ -3,14 +3,14 @@
 // Get the name, phone number, and city from the HTML form
 $name = $_POST['name'];
 $phone = $_POST['phone'];
-$town = $_POST['town'];
+$city = $_POST['city'];
 
 // Set the email subject and body
 $subject = 'Contact Form Submission';
-$body = "Имя: $name\nТелефон: $phone\nГород: $town";
+$body = "Имя: $name\nТелефон: $phone\nГород: $city";
 
 // Send the email
-mail('21itzs.kovalenko.a@pdu.by', $subject, $body);
+mail('email@address.com', $subject, $body);
 
 // Redirect the user to a confirmation page
 header('Location: confirmation.php');
