2e103cba93d1278c089ed305249ea218229dff8b
Aliona Kovalenko <kovalenkoalena789@gmail.com>
2023-07-25 12:31:37 +0300

    update mail

--- sendMail.php
+++ sendMail.php
@@ -1,25 +1,18 @@
 <?php
-if(isset($_POST['name']) && isset($_POST['phone']) && isset($_POST['town'])) {
-    $name = $_POST['name'];
-    $phone = $_POST['phone'];
-    $town = $_POST['town'];
 
-    $to = 'kovalenkoaliona82@gmail.com'; 
-    $subject = 'Новое письмо с формы';
-    $message = "Имя: $name\nТелефон: $phone\nГород: $town";
-    $headers = "From: 21itzs.kovalenko.a@pdu.by\r\n";
-    $headers .= "Content-Type: text/plain; charset=utf-8\r\n";
+// Get the name, phone number, and city from the HTML form
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$town = $_POST['town'];
 
-    if(mail($to, $subject, $message, $headers)) {
-        echo "Письмо успешно отправлено.";
-        header("Location: " . "success.html");
-        exit();
-    } else {
-        echo "Ошибка при отправке письма.";
-        header("Location: " . "index.html");
-        exit();
-    }
-}
+// Set the email subject and body
+$subject = 'Contact Form Submission';
+$body = "Имя: $name\nТелефон: $phone\nГород: $town";
+
+// Send the email
+mail('21itzs.kovalenko.a@pdu.by', $subject, $body);
+
+// Redirect the user to a confirmation page
+header('Location: confirmation.php');
 
 ?>
-
